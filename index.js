635380c3b770848f2f185870e90955e5c789bb39
massi <massi@laptop21.(none)>
2014-12-17 18:16:21 +0100

    iRODS Rest strategy for data management implemented

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * @author Massimiliano Izzo
+ */
+var IrodsRestStrategy = require('./lib/IrodsRestStrategy.js');
+var FileSystemManager = require('./lib/FileSystemManager.js');
+module.exports.IrodsRestStrategy = IrodsRestStrategy;
+module.exports.FileSystemManager = FileSystemManager;
